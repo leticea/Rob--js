fb833184b7fd36541a2f36c57c0e9c7901326a1b
leticea <nascimento.leticeah@gmail.com>
2022-10-29 13:36:31 -0300

    editando a funçao

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ async function robot() {
 
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({path: 'example.png'});
+    const baseCoin = 'dolar';
+    const finalCoin = 'real';
+    const anyUrl = `https://www.google.com/search?q=${baseCoin}+para+${finalCoin}&oq=${baseCoin}+para+&aqs=chrome.0.0i131i433i512j69i57j0i433i512l2j0i512j0i131i433i512l2j0i512l3.2073j1j15&sourceid=chrome&ie=UTF-8`;
+    await page.goto(anyUrl);
+    //await page.screenshot({path: 'example.png'});
 
+    const result =
+
+    console.log(`O valor de 1 ${baseCoin} em ${finalCoin} é ${result}`);
     await browser.close();
 }
 
