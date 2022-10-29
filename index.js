73937666cfb4cdfc199e5da1dc2cab2e05de3812
leticea <nascimento.leticeah@gmail.com>
2022-10-29 14:11:17 -0300

    inserindo readline sync

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ console.log('Bem-vindo ao Bot conversor');
 
 async function robot() {
 
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     const baseCoin = readlineSync.question('Informe uma moeda base:') || 'dolar';
     const finalCoin = readlineSync.question('Informe uma moeda desejada:') || 'real';
+
     const anyUrl = `https://www.google.com/search?q=${baseCoin}+para+${finalCoin}&oq=${baseCoin}+para+&aqs=chrome.0.0i131i433i512j69i57j0i433i512l2j0i512j0i131i433i512l2j0i512l3.2073j1j15&sourceid=chrome&ie=UTF-8`;
     await page.goto(anyUrl);
     //await page.screenshot({path: 'example.png'});
@@ -23,5 +24,3 @@ async function robot() {
 }
 
 robot();
-
-
