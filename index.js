c695862d3c0c37939b7ee188dc1c691f032f4b24
leticea <nascimento.leticeah@gmail.com>
2022-10-29 13:17:55 -0300

    editando a funçao

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const puppeteer = require('puppeteer');
 
 console.log('Bem-vindo ao Bot conversor');
 
-(async () => {
+async function robot() {
+
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://example.com');
     await page.screenshot({path: 'example.png'});
 
     await browser.close();
-})();
+}
+
+robot();
+
+
